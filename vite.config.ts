b023d782e4b18c90e67c7a6143835d7c6ed0d392
Raul Gabriel Bertotto Matos <rgabrielbertottomatos@gmail.com>
2026-05-29 16:28:32 -0300

    atualização

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/arqmanager_frontend",
+  base: "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -24,4 +24,28 @@ export default defineConfig({
       },
     },
   },
+build: {
+    chunkSizeWarningLimit: 700,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const normalizedId = id.replace(/\\+/g, '/')
+
+            if (normalizedId.includes('react-dom')) return 'react-dom'
+            if (normalizedId.includes('react-router-dom')) return 'router'
+            if (normalizedId.includes('@tanstack/react-query')) return 'react-query'
+            if (normalizedId.includes('date-fns')) return 'date-fns'
+            if (normalizedId.includes('axios')) return 'axios'
+            if (normalizedId.includes('lucide-react')) return 'lucide-react'
+            if (normalizedId.includes('zod')) return 'zod'
+            if (normalizedId.includes('@radix-ui')) return 'radix-ui'
+
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
+
 })
